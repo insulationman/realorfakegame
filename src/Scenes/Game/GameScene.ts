271ef8456452a5e6b46ef7347e2b0b974e1c263f
Adam Liljenberg <adam@cabanossy.se>
2022-12-19 16:33:37 +0100

    Can now move player

--- src/Scenes/Game/GameScene.ts
+++ src/Scenes/Game/GameScene.ts
@@ -4,62 +4,79 @@ export default class GameScene extends Phaser.Scene {
   constructor() {
     super("game-scene");
   }
+  private map!: Tilemaps.Tilemap;
+  private layer!: Tilemaps.TilemapLayer;
+  private player!: Phaser.Physics.Arcade.Sprite;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
   preload() {
-    // this.load.image({
-    //   key: "tiles",
-    //   url: "src/Assets/Tilemaps/Tiles/tiles.png",
-    // });
-    // this.load.tilemapTiledJSON("map", "src/Assets/Tilemaps/Json/Map.json");
-
+    //Load tilemap
     this.load.tilemapTiledJSON(
       "Ground",
       "src/Assets/Tilemaps/Json/hagudden-1.json"
     );
-
-    //  Next we load the tileset. This is just an image, loaded in via the normal way we load images:
-
+    //Load tileset
     this.load.image(
       "tiles",
       "src/Assets/Tilemaps/Tiles/Serene_Village_32x32.png"
     );
+    //Load player
+    this.load.image("player", "src/Assets/Sprites/player.png");
   }
 
   create() {
     this.initMap();
-    // this. = new Player(this, 100, 100);
+    this.initPlayer();
+    this.initControls();
+    this.initCameraFollow();
   }
 
-  private map!: Tilemaps.Tilemap;
-  // private tileset!: Tilemaps.Tileset;
-  // private wallsLayer!: Tilemaps.TilemapLayer;
-  // private groundLayer!: Tilemaps.TilemapLayer;
-  private layer!: Tilemaps.TilemapLayer;
+  update() {
+    this.updatePlayer();
+  }
 
   private initMap(): void {
-    // this.scene..backgroundColor = '#787878';
-
-    //  The 'mario' key here is the Loader key given in game.load.tilemap
+    //add the tilemap
     this.map = this.add.tilemap("Ground");
-
-    //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
-    //  The second parameter maps this name to the Phaser.Cache key 'tiles'
+    //add the tileset
     this.map.addTilesetImage("villagetileset", "tiles");
-
+    //create the layer
     this.layer = this.map.createLayer("Ground", "villagetileset", 0, 0);
-    // this.map = this.make.tilemap({
-    //   key: "map",
-    // });
+  }
 
-    // this.tileset = this.map.addTilesetImage("Map", "tiles", 16, 16, 0, 0);
-    // this.groundLayer = this.map.createLayer("Ground", this.tileset, 0, 0);
-    // console.log(this.groundLayer);
-    // this.wallsLayer = this.map.createLayer("Walls", this.tileset, 0, 0);
-    // this.physics.world.setBounds(
-    //   0,
-    //   0,
-    //   this.wallsLayer.width,
-    //   this.wallsLayer.height
-    // );
+  private initPlayer(): void {
+    //add the player
+    this.player = this.physics.add.sprite(50, 50, "player");
+    //set the player to collide with the layer
+    // this.layer.setCollisionByProperty({ collides: true });
+    // this.physics.add.collider(this.player, this.layer);
+  }
+
+  private initControls(): void {
+    //add the controls
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  private updatePlayer(): void {
+    //move the player
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+    } else {
+      this.player.setVelocityX(0);
+    }
+    if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-160);
+    } else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(160);
+    } else {
+      this.player.setVelocityY(0);
+    }
+  }
+
+  private initCameraFollow(): void {
+    //update the camera
+    this.cameras.main.startFollow(this.player);
   }
 }
