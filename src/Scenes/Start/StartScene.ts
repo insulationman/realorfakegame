768b7ddfd4c1c0d59c9b3cedc8781bdf9552d638
Adam Liljenberg <adam@cabanossy.se>
2022-12-20 09:43:27 +0100

    Added intro text

--- src/Scenes/Start/StartScene.ts
+++ src/Scenes/Start/StartScene.ts
@@ -5,6 +5,7 @@ export default class StartScene extends Phaser.Scene {
     super("start");
   }
 
+  private label!: Phaser.GameObjects.Text;
   preload() {
     this.load.image("logo", "src/Assets/Images/Rof-300-400.png");
   }
@@ -12,21 +13,56 @@ export default class StartScene extends Phaser.Scene {
   create() {
     //set background color
     this.cameras.main.setBackgroundColor("#FFFFFF");
-    // this.add.text(20, 20, "Loading..");
-    const logo = this.add.image(0, 0, "logo");
-    //center image dynamically
-    Phaser.Display.Align.In.Center(
-      logo,
-      this.add.zone(
-        document.body.clientWidth / 2,
-        document.body.clientHeight / 2,
-        document.body.clientWidth,
-        document.body.clientHeight
-      )
+
+    this.label = this.add.text(100, 100, "", {
+      font: "70px Arial",
+      color: "#000000",
+      wordWrap: { width: document.body.clientWidth - 200 },
+    });
+
+    this.typewriteTextWrapped(
+      "Greetings traveller! \n......\nYou will go through a journey to find the lost treasure of the ancient kingdom of Real or Fake. \n......\nAre you ready?\n" +
+        "Press any of the arrow keys or touch the screen to start."
     );
 
-    setTimeout(() => {
+    this.input.keyboard.on("keydown", () => {
       this.scene.start("game-scene");
-    }, 1000);
+    });
+
+    this.input.on("pointerdown", () => {
+      this.scene.start("game-scene");
+    });
+
+    //This code will center the Real or fake logo dynamically
+    // const logo = this.add.image(0, 0, "logo");
+    //center image dynamically
+    // Phaser.Display.Align.In.Center(
+    //   logo,
+    //   this.add.zone(
+    //     document.body.clientWidth / 2,
+    //     document.body.clientHeight / 2,
+    //     document.body.clientWidth,
+    //     document.body.clientHeight
+    //   )
+    // );
+  }
+  typewriteTextWrapped(text) {
+    const lines = this.label.getWrappedText(text);
+    const wrappedText = lines.join("\n");
+
+    this.typewriteText(wrappedText);
+  }
+
+  typewriteText(text) {
+    const length = text.length;
+    let i = 0;
+    this.time.addEvent({
+      callback: () => {
+        this.label.text += text[i];
+        ++i;
+      },
+      repeat: length - 1,
+      delay: 50,
+    });
   }
 }
