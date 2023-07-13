46c7945b8d9299e94b91fdec3a3c78851a3c87ce
Adam Liljenberg <adam@cabanossy.se>
2023-07-13 23:37:52 +0200

    now supporting vite build to docs folder

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  //set assetsInclude to include the file types you want to import
+
+  // Define `base` because this deploys to user.github.io/repo-name/
+  base: "./",
+  build: {
+    outDir: "./docs",
+  },
+});
